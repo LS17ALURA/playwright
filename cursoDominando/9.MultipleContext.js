6494f8414903590836d487316204174fb5e36a8f
Lavinia <lavinia.sandi17@gmail.com>
2023-03-10 17:01:52 -0300

    renomeado timeout

--- cursoDominando/9.MultipleContext.js
+++ cursoDominando/9.MultipleContext.js
@@ -1,7 +1,7 @@
 const { chromium } = require('playwright');
 
 (async () => {
-    const browser = await chromium.launch()
+    const browser = await chromium.launch({headless: false, slowMo: 1000 })
 
     const userContext = await browser.newContext()
     const adminContext = await browser.newContext()
